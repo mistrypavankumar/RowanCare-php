264a3fb81834f704613b817f99c0a0fbc3dbef57
Mistry Pavan Kumar <68140538+mistrypavankumar@users.noreply.github.com>
2023-11-22 20:24:18 -0500

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "7143@Pavan";
-$dbname = "mistry64";
+$password = "yourmysqlpassword";
+$dbname = "databaseName";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
